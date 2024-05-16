7c7bab560a4ba27ddaad1b261aeac098d549fb50
Trevor Sandoval <162456207+Trevor-Sandoval@users.noreply.github.com>
2024-05-16 18:34:13 -0500

    Update chart.sql

--- data/chart.sql
+++ data/chart.sql
@@ -1,36 +1,84 @@
-Department
--
-Dept_No VARCHAR PK FK >- Dept_emp.Dept_No
-Dept_Name VARCHAR
-Dept_emp
--
-Emp_No int FK >- Employees.Emp_No
-Dept_No VARCHAR
-Dept_Manager
--
-Dept_No VARCHAR FK >- Department.Dept_No
-Emp_No int FK >- Salaries.Emp_No
-Employees
--
--
-Emp_No int FK >- Dept_Manager.Emp_No
-Emp_Title_ID VARCHAR FK >- Titles.Title_ID
-Birth_Date VARCHAR
-First_Name VARCHAR
-Last_Name VARCHAR
-Sex VARCHAR
-Hire_Date
-Salaries
--
-Emp_No int FK >- Dept_emp.Emp_No
-Salary int
-Titles
--
-Title_ID VARCHAR
-Title VARCHAR
+-- Table creation
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+  dept_no varchar PRIMARY KEY,
+  dept_name varchar NOT NULL
+);
+CREATE TABLE titles (
+  title_id varchar PRIMARY KEY,
+  title varchar NOT NULL
+);
+CREATE TABLE employees (
+  emp_no integer PRIMARY KEY,
+  emp_title_id varchar NOT NULL,
+  birth_date date NOT NULL,
+  first_name varchar NOT NULL,
+  last_name varchar NOT NULL,
+  sex varchar NOT NULL,
+  hire_date date NOT NULL,
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+CREATE TABLE dept_emp (
+  emp_no integer NOT NULL,
+  dept_no varchar NOT NULL,
+  PRIMARY KEY (emp_no, dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+CREATE TABLE dept_manager (
+  dept_no varchar NOT NULL,
+  emp_no integer NOT NULL,
+  PRIMARY KEY (dept_no, emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+CREATE TABLE salaries (
+  emp_no integer NOT NULL,
+  salary integer NOT NULL,
+  PRIMARY KEY (emp_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+-- SELECT queries
+SELECT emp_no, last_name, first_name, sex, salary
+FROM employees
+JOIN salaries USING(emp_no);
 
+SELECT first_name, last_name, hire_date
+FROM  employees
+WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
 
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
 
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM  employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
 
+SELECT first_name, last_name, sex
+FROM  employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
+SELECT e.emp_no, e.last_name, e.first_name
+FROM  employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM  employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency
+FROM  employees
+GROUP BY last_name
+ORDER BY frequency DESC;
